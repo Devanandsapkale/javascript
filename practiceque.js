fc8edde991a505ef241418effeb6c457148ba363
Devanand Sapkale <kolideva54@gmail.com>
2024-02-19 01:54:36 +0530

    for loop
    program for...
    
    1) print  all even number
    2) print all odd number

---
+++ practiceque.js
@@ -0,0 +1,16 @@
+// Que 1, print all even numbers from 0 to 100
+
+for(let i=1;i<=100;i++){
+    if(i%2===0){
+        document.write("even num = ",i +"<br>");
+    }
+    
+}
+
+//Que 2, print all odd number from 0 to 100
+
+for(let num=0; num<=100;num++){
+    if(num%2!==0){
+        document.write("odd num= ",num+"<br>");
+    }
+}
