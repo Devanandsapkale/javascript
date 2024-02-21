c6f5e84dc2e83afe1fbf6bfda7a88fbdcad2eb5f
Devanand Sapkale <kolideva54@gmail.com>
2024-02-21 23:30:31 +0530

    String practice code

--- strings.js
+++ strings.js
@@ -1,4 +1,23 @@
 // Strings
+let str="ApnaCollege";
+str.toUpperCase()// method not change the orignal string its creat a new value
+let newstr=str.toUpperCase()// 
+console.log(str);
+console.log(newstr);
+//slicing in string
+let str1="devanand";
+console.log(str1.slice(1,4));
+//concatination method
+let str2="Dev";
+let str3="koli";
+let str4= str2.concat(str3);
+console.log(str4);
+//second method to concatination
+let a="dev";
+let b="koli";
+let name=a+b;
+console.log(name);
+//replace method
 
-let str = "devanand";
-document.write(str[4]); //for find the indext of the string we use "str[]" and put the indext number in the squre bracket
+let string="Hello";
+console.log(string.replace("lo","p"));
