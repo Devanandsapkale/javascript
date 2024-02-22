ff28ded273167b7747755c0e0572b0f0263a6dbe
Devanand Sapkale <kolideva54@gmail.com>
2024-02-23 01:50:20 +0530

    Function practice code

---
+++ functions.js
@@ -0,0 +1,19 @@
+// Function...
+function myfunction(){
+    console.log("Welcome to my profile");
+    console.log("We are learning js");
+}
+myfunction();
+//creat a function for sum of two number
+
+function sum(a ,b){
+    console.log(a+b);
+}
+//multiplication function
+function mul(x,y){
+    return x * y;
+}
+//arrow functions
+const arrowSum = (a,b) => {
+    console.log(a + b);
+}
