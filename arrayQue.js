749730caa0d5b6cbb0a5cb4cac1202934753167b
Devanand Sapkale <kolideva54@gmail.com>
2024-02-22 17:52:26 +0530

    Array que based on methods

--- arrayQue.js
+++ arrayQue.js
@@ -16,15 +16,33 @@ all items have an offer of 10% off on them changes the array to store
 final price after applying offer*/
 
 let item = [250,645,300,900,50]; 
-let i=1;
+let i=0;
 for(let val of item){
     let offer =val/10;
     item[i] = item[i]- offer;
     console.log(`value at offer = ${item[i]}`);
     i++;
 }
-// for(let i=0; i<item.length;i++){
-
-// }
 console.log(i);
+
+//use  for loop
+let items=[200,300,400,500,600,700,]
+for(let i=0; i<items.length;i++){
+    let offer = items[i]/10;
+    items[i]-=offer;
+
+}
+console.log(items);
+
+/*create an array to store companies-->bloomberg,microsoft,uber,google,ibm,netfilix
+a. romove the first company from the array
+b. remove uber and add ola in its place 
+c. add amazon at the end*/
+
+let companies =["Bloomberg","Microsoft","Uber","Google","IBM","Netflix"];
+companies.shift();
+companies.splice(1,1,"ola");
+companies.push("Amazon");
+console.log(companies);
+
     
