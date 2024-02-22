88934fb31a905fa2c866430c8309f54028dbfd51
Devanand Sapkale <kolideva54@gmail.com>
2024-02-22 17:52:00 +0530

    array methods

--- array.js
+++ array.js
@@ -19,3 +19,32 @@ let cities=["delhi","mumbai","pune","hydrabad","jalgaon"];
 for(let city of cities){
     console.log(city.toUpperCase());
 }
+
+let food=["apple","banana","mango","litchi"];
+food.push("orange");
+food.pop();
+console.log(food);
+
+let marvel=["thor","spiderman","ironman"];
+marvel.unshift("antman"); //umshipt method use to add the element at the starting.
+marvel.shift(); // shift method use to delete value of the starting
+let dc=["superman","mbatman"];
+let marvel_dc = marvel.concat(dc);
+console.log(marvel_dc);
+
+//slice method of array
+let color=["blue","orange","pink","yellow","red","black"];
+console.log(color);
+console.log(color.slice(2,4));
+
+//splice methd "array"
+let arr =[1,2,3,4,5,6,7,8,];
+arr.splice(2,2,101,102);
+//add element by using splice
+arr.splice(5,0,103); 
+//delete element by using splice
+arr.splice(4,1);
+//Replace element by using splice
+arr.splice(5,1,104);
+console.log(arr);
+
