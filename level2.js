d2764624bb5612a0043136195226281dfca3d00f
Devanand Sapkale <kolideva54@gmail.com>
2024-03-19 23:53:54 +0530

    Update level2.js

--- level2.js
+++ level2.js
@@ -47,3 +47,34 @@ let student ={
 for(let i in student){
     console.log("i =", i,"value",student[i]); //for get the i value use "value",student[i]
 }
+
+//function:-type of functio
+// fuction ----- simple function
+// arow function
+// anonymous function
+
+
+
+function add(a, b) {         
+    console.log(a + b);
+}
+ 
+// Calling a function
+add(2, 3);
+
+// Function Expression
+const add = function (a, b) {
+    console.log(a + b);
+}
+ 
+// Calling function
+add(2, 3);
+
+
+const great = (a, b) => {
+    if (a > b)
+        return "a is greater";
+    else
+        return "b is greater";
+}
+console.log(great(3, 5));
