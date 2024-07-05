cc65e4985b91a810f732cb5a2e005343152a11f0
Devanand Sapkale <kolideva54@gmail.com>
2024-07-06 00:32:32 +0530

    practice code

---
+++ DOM OBJ/day2.js
@@ -0,0 +1,5 @@
+let box=document.querySelectorAll(".box");
+box[0].innerText="new unique value 1"; 
+box[1].innerText="new unique value 2"; 
+box[2].innerText="new unique value 3"; 
+console.log(box);
