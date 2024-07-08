47093da6396aece96e9dbe3a2e0cc419b9eb8254
Devanand Sapkale <kolideva54@gmail.com>
2024-07-09 02:06:08 +0530

    practice code

--- event/que1.js
+++ event/que1.js
@@ -1,6 +1,7 @@
 //Que-1  toggling effect white to dark and dark to white;
 let mode = document.querySelector("#mode");
 let body=document.querySelector("*");
+let badak=document.querySelector("#badak");
 
 let light="white";
 mode.addEventListener("click",()=>{
@@ -14,23 +15,26 @@ mode.addEventListener("click",()=>{
     console.log(light);
 }
 })
-//Que-2  pollendram program
+//Que-2  pollendrom program
 
 
-mode.addEventListener("mouseover",pollendram);
-function pollendram(str){
+badak.addEventListener("click",() =>{
     let num = document.querySelector("#num").value;
     let num1=num.split("");
-    num1+=num1.reverse().join("");
-    console.log(num1==num);
+    let num2=num1.reverse();
+    let ans=num2.join("");
+    if(ans==num){
+        console.log("This is 1");
+    }else{
+        console.log("This is 2");
+    }
+    
+});
 
-}
+    
+    
 
 
-//Que-3 make changes by using mouseover and mouseout
-let div1=document.querySelector("#d1");
-let div2=document.querySelector("#d2");
-div1.addEventListener("mouseover",()=>{
-    div1.classList.add(body);
-    console.log("we in");
-})
+
+
+
