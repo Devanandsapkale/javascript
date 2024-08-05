9bb2f281eb16d26c6252a19f1e7034fc44637cd0
Devanand Sapkale <kolideva54@gmail.com>
2024-08-06 00:57:20 +0530

    variable in js

---
+++ js/first.js
@@ -0,0 +1,11 @@
+// nodejs is a 
+
+
+
+//console.log("hello js");
+
+
+let arr = [1,2,3,4,5];
+let arr1 = arr.splice(1,3);
+console.log(arr1);
+console.log(arr);
---
+++ js/variable/variable.js
@@ -0,0 +1,20 @@
+
+
+
+var name = "Devanand sapkale";
+console.log(name);
+var city = "pune"; 
+console.log(city);
+const age = "18";
+console.log(age);
+let username ="devkoli";
+console.log(username);
+let date ="22/05/2004";
+console.log(date);
+let color ="orange,blue,red";  
+console.log(color);
+
+console.table([name,city,username,date,color]);
+
+
+
