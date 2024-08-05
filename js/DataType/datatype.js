2443a94abc8dedbf6d8a332ad45c547a3f149cd5
Devanand Sapkale <kolideva54@gmail.com>
2024-08-06 00:57:45 +0530

    Data Type In Js

---
+++ js/DataType/datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js as newr version of js its not required 
+
+//alert(3 + 3)  // its not work becouse we using nodejs, not browser
+
+
+
+
+//primitive data type
+
+// number -------- range 2 to power 53
+// string -------- " "
+// boolean ------- true/false
+// bigint -------- its use if you have big number
+// null ---------- standalone value
+// undefined ----- not defined
+// symbol --------- unique
+
+//Non primitive data type
+
+//object
+ let city = 'pune';
+ let rollNum = 33;
+ let present = true;
+ let value = null;
+ let pass;
+ console.table(typeof(city));
+ console.table(typeof(rollNum));
+ console.table(typeof(present));
+ console.table(typeof(value));
+ console.table(typeof(pass));
