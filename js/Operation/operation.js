da308d5cacd664dae18b0f9a167a02a731596333
Devanand Sapkale <kolideva54@gmail.com>
2024-08-06 01:31:58 +0530

    Operators in js

---
+++ js/Operation/operation.js
@@ -0,0 +1,26 @@
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2); // power 2
+console.log(2%2);  // reminder
+
+let str1 = "dev";
+let str2 = "koli";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);  // it will be concad
+console.log(1 + "2");  // it will be concad
+
+let score = 100;
+score++
+console.log(score);
+
+let mark = 90;
+mark++
+console.log(mark);
+
+
+
