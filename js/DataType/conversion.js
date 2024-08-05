fa71c0babfe365d26bdf22c0eabecd3f20b089b7
Devanand Sapkale <kolideva54@gmail.com>
2024-08-06 00:58:36 +0530

    Conversion of data types

---
+++ js/DataType/conversion.js
@@ -0,0 +1,20 @@
+let isLoggedIn = "Dev";
+let booleanInLoggedIn = Boolean(isLoggedIn);
+console.log(booleanInLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"dev" => true
+
+let score = '22'
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 ; flase => 0
+
+let someNumber = '22'
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof  stringNumber);
