4586b0fec48328f55fc4c9653ef6ccb13fbcd1bf
Devanand Sapkale <kolideva54@gmail.com>
2024-08-08 17:26:59 +0530

    String in js practice and math and number practice in js

--- js/DataType/string.js
+++ js/DataType/string.js
@@ -11,3 +11,23 @@ console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('m'));
 
+let str = "devanand";
+let  str1 = str.substring(2,7);
+ console.log(str);
+ console.log(str1);
+
+//  let str2 = str.slice(1,4);
+//  console.log(str2);
+ let str2 = str.slice(-5,8);
+ console.log(str2);
+
+let msg = "hello-i am-ram";
+console.log(msg.includes("ram"));
+let newMsg = msg.split("-");
+console.log(newMsg);
+
+
+
+ 
+ 
+ 
---
+++ js/num-Math/numAndMath.js
@@ -0,0 +1,34 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 123.8464;
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+
+
+// //++++++++++++++++++math++++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5)); //----- absulate value by using abs we cam make "nagative value to positive"
+// console.log(Math.round(7.8));
+// console.log(Math.ceil(7.8));
+// console.log(Math.floor(7.8));
+// console.log(Math.min(5,7,2,6,8,9,3,));
+// console.log(Math.max(5,7,2,6,8,9,3,));
+
+console.log(Math.random());
+console.log(Math.random());
+
+
+
+
+
+
