9653647a32ac04b27cd25d4a95c612de31d8d129
Devanand Sapkale <kolideva54@gmail.com>
2024-08-15 23:57:41 +0530

    arrow function practice

---
+++ js/function/arrow.js
@@ -0,0 +1,33 @@
+const user = {
+    username: "ram",
+    price: 999,
+    welcomeMsg: function(){
+        console.log(`${this.username}, welcome to website`);   
+            
+    }
+
+}
+// user.welcomeMsg()
+// user.username ="sam"
+// user.welcomeMsg()
+// console.log(this);
+
+const fun1 = ()=>{
+    let username ="sham"
+    // console.log(this);   
+}
+fun1()
+
+// const addTwo = (num1,num2) => {      //explicit return, 
+//     return num1 + num2
+// }
+
+//const addTwo = (num1,num2) => num1 + num2 // implicit return
+
+// const addTwo = (num1,num2) => (num1 + num2) // implicit return
+
+const addTwo = (num1,num2) => ({username: "sham"}) // object return
+
+
+console.log(addTwo(5,2));
+
--- js/function/scope.js
+++ js/function/scope.js
@@ -5,3 +5,28 @@
  console.log(b);
  console.log(c);
  
+function one(){
+    const username = "Deva"
+    
+    function two(){
+        const website = "youtube"
+        console.log(username);        
+    }
+    // console.log(website);
+    two()
+}
+one()
+
+//+++++++++++++++++++++interesting+++++++++++++++++++++++++++++
+
+console.log(addone(4)) // Before declaration allow
+function addone(num){
+    return num + 1;
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5)); // Before declaration not allow
+ 
