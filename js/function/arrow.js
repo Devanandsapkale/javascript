037652ecf80250d690fb3e171aedc4f129a4385d
Devanand Sapkale <kolideva54@gmail.com>
2024-08-16 00:11:09 +0530

    IIFE concept of js

--- js/function/arrow.js
+++ js/function/arrow.js
@@ -31,3 +31,14 @@ const addTwo = (num1,num2) => ({username: "sham"}) // object return
 
 console.log(addTwo(5,2));
 
+
+// Immediately Invoked Function Expression (IIFE)-----------------------
+
+(function chai(){                // named iife , fun have name 
+    console.log(`DB CONNECTED`);
+})();
+
+(() =>{                         // unnemed iife , fun not have name
+    console.log(`DB CONNECTED`);
+})();
+
